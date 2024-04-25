1820147e9caec482e6d72bc8ce7d3938d34b04d4
tahul <taulwwks@gmail.com>
2024-04-25 15:04:42 +0700

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+//<!-- Footer -->
+<div className="w-full flex-grow-0 items-center justify-between gap-3 border-b border-gray-200 bg-white px-4 py-3 dark:border-gray-600 dark:bg-white/[0.15] md:border-b-0 md:bg-transparent">
+  <div className="w-full flex-grow-0 items-center justify-between gap-3 border-b border-gray-200 bg-white px-4 py-3 dark:border-gray-600 dark:bg-white/[0.15] md:border-b-0 md:bg-transparent">
+  <div className="w-full flex-grow-0 items-center justify-between gap-3 border-b border-gray-200 bg-white px-4 py-3 dark:border-gray-600 dark:bg-white/[0.15] md:border-b-0 md:bg-transparent">
+      <span>&copy; Panti Asuhan Baitii Jannati</span>
+      
+    </div>
+  </div>
+</div>
+//End of Footer
+export default function Page() {
+    return <p>Dashboard Page</p>;
+}
---
+++ app/ui/fonts.tsx
@@ -0,0 +1,5 @@
+import { Inter } from 'next/font/google';
+export const lusitana = {
+    // your font definition here
+  };
+export const inter = Inter({ subsets: ['latin'] });
